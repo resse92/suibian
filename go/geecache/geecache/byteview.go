8f92a2cbd89683637aba343908a14b6aefd70ec0
zhuruliang <zrl920825@hotmail.com>
2020-03-27 09:28:52 +0800

    geecache day4

---
+++ go/geecache/geecache/byteview.go
@@ -0,0 +1,25 @@
+package geecache
+
+type ByteView struct {
+	b []byte
+}
+
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+// Byte Slice return a copy of the data as a byte slice
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+// string returns the data as a string making a copy if necessary
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
